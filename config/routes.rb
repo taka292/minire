e7310d0746e5774000842a71a37ba350d9e58747
taka292 <66032836+taka292@users.noreply.github.com>
2024-12-03 08:03:29 +0900

    Deviseのセットアップと、簡易的なログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  # devise_for :users
   get "home/index"
   root 'home#index'
   # get 'search', to: 'reviews#search'
