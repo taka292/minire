fc3db98dd61ffe0d341709d0b0993dbef5594aa3
taka292 <66032836+taka292@users.noreply.github.com>
2024-12-17 20:30:19 +0900

    ユーザーアバター画像の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reviews, dependent: :destroy
+  has_one_attached :avatar
+
   # バリデーション
   validates :name, presence: true
+  validate :avatar_content_type
+  validate :avatar_size
+
+  def avatar_content_type
+    allowed_types = %w[image/jpeg image/png image/gif]
+    if avatar.attached? && !avatar.content_type.in?(allowed_types)
+      errors.add(:avatar, 'ファイル形式はJPEG, PNG, GIFのみアップロード可能です。')
+    end
+  end
+
+  def avatar_size
+    if avatar.attached? && avatar.blob.byte_size > 5.megabytes
+      errors.add(:avatar, '：5MB以下のファイルをアップロードしてください。')
+    end
+  end
+
+  # リサイズした画像を返すメソッド
+  def resized_avatar
+    avatar.variant(resize_to_fill: [100, 100]).processed if avatar.attached?
+  end
 end
