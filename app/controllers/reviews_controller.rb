03512d53fa81e1a5be01ddaa55a14607f123fcc5
taka292 <66032836+taka292@users.noreply.github.com>
2024-12-22 12:03:50 +0900

    レビューコントローラーの認証を更新し、indexおよびshowアクションへのアクセスを認証なしで許可

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [ :index, :show ]
   before_action :set_review, only: %i[edit update destroy]
 
   def new
