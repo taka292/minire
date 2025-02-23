ccbc1072605808e5966c76a96d3ee433cc24865f
taka292 <66032836+taka292@users.noreply.github.com>
2025-02-23 23:28:50 +0900

    コメントの作成と削除機能を非同期で実装し、コメント表示のスタイルを改善

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,33 +1,51 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_comment, only: [ :destroy ]
+  before_action :find_commentable, only: %i[create]
+  before_action :set_comment, only: %i[destroy]
 
   def create
-    @review = Review.find(params[:review_id])
-    @comment = @review.comments.build(comment_params.merge(user: current_user))
-    if @comment.save
-      redirect_to @review, notice: "コメントを投稿しました！"
-    else
-      redirect_to @review, alert: "コメントの投稿に失敗しました。"
+    @comment = @commentable.comments.build(comment_params.merge(user: current_user))
+    @comments = @commentable.comments.includes(:user)
+
+    respond_to do |format|
+      if @comment.save
+        format.turbo_stream
+        format.html { redirect_to @commentable }
+      else
+        # format.turbo_stream do
+        #   flash.now[:danger] = t('defaults.message.not_created', item: Comment.model_name.human)
+        # render turbo_stream: [
+        #   turbo_stream.replace("flash_messages", partial: "shared/flash_messages"),
+        # ]
+        # end
+        format.html { redirect_to @commentable }
+      end
     end
   end
 
   def destroy
     if @comment.user == current_user
       @comment.destroy
-      redirect_to @comment.review, notice: "コメントを削除しました。"
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to @comment.commentable, notice: "コメントを削除しました。" }
+      end
     else
-      redirect_to @comment.review, alert: "コメントの削除権限がありません。"
+      redirect_to @comment.commentable, alert: "コメントの削除権限がありません。"
     end
   end
 
   private
 
   def set_comment
-    @comment = Comment.find(params[:id])
+    @comment = current_user.comments.find(params[:id])
   end
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def find_commentable
+    @commentable = Review.find(params[:review_id])
   end
 end
