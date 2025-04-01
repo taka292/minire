7a02d7e81d07acf88f36d2724ec7cc9a103c7079
taka292 <66032836+taka292@users.noreply.github.com>
2025-04-01 11:58:51 +0900

    レビュー投稿機能のテストを強化し、編集画面での手放せるものの削除および画像の追加・削除機能を実装

--- spec/support/system_helper.rb
+++ spec/support/system_helper.rb
@@ -1,8 +1,13 @@
 module SystemHelper
   def login(user)
     visit new_user_session_path
+
+    expect(page).to have_selector("input[name='user[email]']", wait: 5)
+
     fill_in "user[email]", with: user.email
     fill_in "user[password]", with: "password"
     click_button "ログイン"
+
+    expect(page).to have_current_path(home_index_path)
   end
 end
