ff10b7c5d58fd18566bee6d08b73987914f74c78
taka292 <66032836+taka292@users.noreply.github.com>
2025-04-08 11:29:34 +0900

    不要なヘルパーとコントローラーのファイルを削除

--- app/javascript/controllers/comment_controller.js
+++
@@ -1,9 +0,0 @@
-// import { Controller } from "@hotwired/stimulus";
-// 
-// export default class extends Controller {
-//   static targets = ["form", "input"];
-// 
-//   resetForm() {
-//     this.inputTarget.value = ""; // 入力フィールドをリセット
-//   }
-// }
