04137118b2b189306f886ea54ba399fedbf0d25c
taka292 <66032836+taka292@users.noreply.github.com>
2025-04-13 22:45:07 +0900

    トップページのにアイコン画像等を追加して、AOSライブラリを追加してアニメーション効果を実装。

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,16 @@ module.exports = {
     extend: {
         colors: {
         customBlue: '#3BB1D8', // カスタムカラー名を定義
-      },
+        },
+        keyframes: {
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        },
+        animation: {
+          fadeInUp: 'fadeInUp 0.6s ease-out forwards',
+        },
     },
   },
   plugins: [require("daisyui")],
