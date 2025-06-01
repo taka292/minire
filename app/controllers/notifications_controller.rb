2c755a98ca12f7c4a0a5a42847855b63f0301bee
taka292 <66032836+taka292@users.noreply.github.com>
2025-06-01 10:45:57 +0900

    refactor: 通知コントローラーの不要なコメントを削除し、ホームページのスタイルの不要なスタイルを削除

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,16 +2,14 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    # @unchecked_notifications = current_user.passive_notifications.where(checked: false).order(created_at: :desc)
+    # 未読通知を取得
     @checked_notifications = current_user.passive_notifications.where(checked: true).order(created_at: :desc).limit(20)
-
-    # 全通知を既読にする
-    # @unchecked_notifications.update_all(checked: true)
 
     # 通知一覧ページをレンダリング
     render "notifications/index"
   end
 
+  # 未読通知を一括既読にする
   def update_checked
     current_user.passive_notifications.update_all(checked: true)
     redirect_to notifications_path, notice: "通知をすべて既読にしました"
