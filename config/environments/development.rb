bc785ca68fba994f05825041d9c5ce1cc16ff2d5
taka292 <66032836+taka292@users.noreply.github.com>
2025-06-10 18:09:18 +0900

    add: Bulletの設定を追加してN+1検出を有効化

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,16 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  # bullet(N + 1検出)の設定
+  config.after_initialize do
+    Bullet.enable        = true
+    Bullet.alert         = true
+    Bullet.bullet_logger = true
+    Bullet.console       = true
+    Bullet.rails_logger  = true
+    Bullet.add_footer    = true
+  end
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Make code changes take effect immediately without server restart.
